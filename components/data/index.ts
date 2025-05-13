ba11eb62885f4f11940de21fafc07365439c98ca
Jannat226 <jannat@mca.christuniversity.in>
2025-05-13 15:28:55 -0400

    updating

--- components/data/index.ts
+++ components/data/index.ts
@@ -8,7 +8,7 @@ export const projects = [
       // Built multiple CNN-based models and an ensemble using PyTorch Lightning.
     img: "/gutBrain.png",
    
-    iconLists: ["python.svg","pytorch.png","huggingFace.png"],
+    iconLists: ["python.svg","pytorch.png","huggingface-color.png","nlp.png"],
     link: "https://github.com/NLPatVCU/GutBrainIE/tree/cnn",
   },
   {
