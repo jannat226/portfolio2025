655465100f95a52de1b25c6522c575ee16af4158
Jannat226 <jannat@mca.christuniversity.in>
2025-05-13 15:15:44 -0400

    updating

--- components/data/index.ts
+++ components/data/index.ts
@@ -13,20 +13,21 @@ export const projects = [
   },
   {
     id: 2,
-    title: "Crawler-Python Script",
-    des: "Automation for downloading the tenders files from the government tender website",
-    img: "/Crawler.png",
-    iconLists: ["python.svg","selenium.svg"],
-    link: "https://bitbucket.org/jannatjiya/karnataka_eprocurement/src/main/tender_test/test_tender.py",
-  },
-  {
-    id: 3,
     title: "Carpooling App",
     des: "Seamlessly connect with colleagues and friends who needs a ride and those who can provide ride to the campus.",
     
     img: "/Ridesharee.png",
     iconLists: ["reactNative.svg", "/firebase.svg", "/tail.svg"],
     link: "https://github.com/jannat226/ridesharee",
+  },
+  {
+    id: 3,
+    title: "Crawler-Python Script",
+    des: "Automation for downloading the tenders files from the government tender website",
+    img: "/Crawler.png",
+    iconLists: ["python.svg","selenium.svg"],
+    link: "https://bitbucket.org/jannatjiya/karnataka_eprocurement/src/main/tender_test/test_tender.py",
+   
   },
   // {
   //   id: 3,
