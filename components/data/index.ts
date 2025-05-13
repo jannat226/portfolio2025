1e97b933222389a1439fc29a81a8b72f7dd20f4d
Jannat226 <jannat@mca.christuniversity.in>
2025-05-13 15:10:28 -0400

    updating

--- components/data/index.ts
+++ components/data/index.ts
@@ -1,16 +1,18 @@
 
 export const projects = [
   {
+    id:1,
     title: "GutBrainIE",
     des:
-      "Designed a relation extraction pipeline for PubMed abstracts and titles focused on gut-brain axis literature.",
-    img: "/public/gutBrain.png",
+      "Designed a relation extraction pipeline focused on gut-brain axis literature.  ",
+      // Built multiple CNN-based models and an ensemble using PyTorch Lightning.
+    img: "/gutBrain.png",
    
     iconlist: ["python.svg","/Icons/pytorch.png","/Icons/huggingFace.png","/Icons/nlp.png"],
     link: "https://github.com/NLPatVCU/GutBrainIE",
   },
   {
-    id: 1,
+    id: 2,
     title: "Crawler-Python Script",
     des: "Automation for downloading the tenders files from the government tender website",
     img: "/Crawler.png",
@@ -18,7 +20,7 @@ export const projects = [
     link: "https://bitbucket.org/jannatjiya/karnataka_eprocurement/src/main/tender_test/test_tender.py",
   },
   {
-    id: 2,
+    id: 3,
     title: "Carpooling App",
     des: "Seamlessly connect with colleagues and friends who needs a ride and those who can provide ride to the campus.",
     
@@ -26,13 +28,13 @@ export const projects = [
     iconLists: ["reactNative.svg", "/firebase.svg", "/tail.svg"],
     link: "https://github.com/jannat226/ridesharee",
   },
-  {
-    id: 3,
-    title: "Time Capsule",
-    des: "An website which can send you or your loved one the reminders of certain memories after the destined time period ",
-    img: "/TimeCapsule.png",
-    iconLists: ["/re.svg", "/tail.svg", "/mongoDb.svg"],
-    link: "https://github.com/jannat226/ESE-Componet--III-WSD-Project-",
-  },
+  // {
+  //   id: 3,
+  //   title: "Time Capsule",
+  //   des: "An website which can send you or your loved one the reminders of certain memories after the destined time period ",
+  //   img: "/TimeCapsule.png",
+  //   iconLists: ["/re.svg", "/tail.svg", "/mongoDb.svg"],
+  //   link: "https://github.com/jannat226/ESE-Componet--III-WSD-Project-",
+  // },
   
 ];
