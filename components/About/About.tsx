a7e7a809b251230dadeb9adf3f85984c7cfdcb81
Jannat226 <jannat@mca.christuniversity.in>
2025-05-13 15:20:20 -0400

    updating

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <p className="section-header">About</p>
       {/* Replace this p tag with more info about yourself. */}
       <p className="main-color ">
-      Hi, I am Jannat , a passionate software engineer with a strong interest in backend development, data structures,  algorithms and Machine Learning. I am currently pursuing my Master of Science in Computer Science at Virginia Commonwealth University. With experience in NLP, React,React Native,Node JS, Python and other modern technologies. I enjoy building efficient, scalable solutions and creating user-friendly applications. When I am not coding, I am exploring new frameworks, honing my skills, or diving into projects that solve real-world problems. Let&aposs connect and create something amazing!
+      Hi, I am Jannat , a passionate software engineer with a strong interest in backend development, data structures,  algorithms and Machine Learning. I am currently pursuing my Master of Science in Computer Science at Virginia Commonwealth University. With experience in NLP, React,React Native,Node JS, Python and other modern technologies. I enjoy building efficient, scalable solutions and creating user-friendly applications. When I am not coding, I am exploring new frameworks, honing my skills, or diving into projects that solve real-world problems.
       </p>
     </div>
   );
