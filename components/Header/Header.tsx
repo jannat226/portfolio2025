6241703fc342341068b06802c4ca9b2132fe6edc
Jannat226 <jannat@mca.christuniversity.in>
2025-08-02 00:28:47 -0400

    file - updated

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Linkedin, Github, FileText } from "lucide-react";
+import { Linkedin, Github, FileText, Mail } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 // import React, { useState, useEffect } from "react";
@@ -50,7 +50,11 @@ export default function Header() {
               <Github size={18} />
               <p className="icon-text">GitHub</p>
             </Link>
-            <Link href="/jannat_Resume.pdf" target="_blank" className="icons">
+            <Link href="mailto:jannatchehal09@gmail.com" className="icons">
+              <Mail size={18} />
+              <p className="icon-text">Email</p>
+            </Link>
+            <Link href="/Jannat_Professional_Resume.pdf" target="_blank" download="Jannat_Resume.pdf" className="icons">
               <FileText size={18} />
               <p className="icon-text">Resume</p>
             </Link>
